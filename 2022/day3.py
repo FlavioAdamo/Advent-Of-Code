f80ff52915963fb603de75f6c851cb125c6c8648
Flavio Adamo <flavioadamo.adams@gmail.com>
2022-12-03 18:58:08 +0100

    day3

---
+++ 2022/day3.py
@@ -0,0 +1,29 @@
+LOWER = 'abcdefghijklmnopqrstuvwxyz'
+UPPER = LOWER.upper()
+
+def get_score(char):
+   if char in LOWER:
+      return LOWER.find(char) +1
+   return UPPER.find(char) + 27
+
+#SOLUTION
+with open('input.txt', 'r') as input_file:
+   rucksacks = input_file.read().split('\n')
+
+chars_list = []
+bedges = []
+for items in rucksacks:
+   common_chars = list(set(items[:len(items)//2]).intersection(items[len(items)//2:]))
+   chars_list.append(get_score(common_chars[0]))
+
+for index in range(0, len(rucksacks[:-2]), 3):
+   common_chars = list(
+         set(rucksacks[index])
+         .intersection(rucksacks[index + 1])
+         .intersection(rucksacks[index + 2])
+   )
+   bedges.append(get_score(common_chars[0]))
+
+print(sum(chars_list))
+print(sum(bedges))
+
